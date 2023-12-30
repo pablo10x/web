98402edc7d57e041cae5d3a9d9ed51574bc4aa65
pablo poco <pabapple@icloud.com>
2023-12-30 19:53:53 +0000

    xx

---
+++ src/lib/Components/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  $: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar>
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl {activeUrl}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/docs/components/navbar">Navbar</NavLi>
+    <NavLi href="/docs/components/accordion">Accordion</NavLi>
+    <NavLi href="/docs/components/alert">Alert</NavLi>
+    <NavLi href="/docs/components/avatar">Avatar</NavLi>
+  </NavUl>
+</Navbar>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,10 @@
+export function load() {
+      
+      console.log("server data")
+  return {
+    post: {
+      title: `Title for goes here`,
+      content: `Content for  goes here`,
+    },
+  };
+}
